a70fed327a3a7e3f45c7b8b8a409a5ec960e41df
Simon Conrad <simon.conrad@mgm-tp.com>
2025-07-11 09:56:30 +0200

    upgrade to Capacitor 7

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ShareExtensionPlugin } from './definitions';
-declare const ShareExtension: ShareExtensionPlugin;
-export * from './definitions';
-export { ShareExtension };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const ShareExtension = registerPlugin('ShareExtension', {
-    web: () => import('./web').then(m => new m.ShareExtensionWeb()),
-});
-export * from './definitions';
-export { ShareExtension };
-//# sourceMappingURL=index.js.map
